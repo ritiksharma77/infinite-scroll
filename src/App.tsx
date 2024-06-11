040b63e0b35bef14fcee281d44a10d26d22926f9
ritiksharma77 <ritiksharma98166@gmail.com>
2024-06-12 00:47:18 +0530

    infinite scroll logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,37 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { ProductCard } from "./components/product-card";
 import { fetchData } from "./components/fetch-data";
 
 function App() {
-  const [data, setData] = useState<any>(new Set());
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const addUniqueItems = (prevSet: any, newItems: any) => {
-    const updatedSet = new Set(prevSet);
-
-    newItems.forEach((item: any) => {
-      if (
-        ![...updatedSet].some(
-          (existingItem: any) => existingItem.id === item.id
-        )
-      ) {
-        updatedSet.add(item);
+  const scrollableDivRef = useRef<any>(null);
+  const [data, setData] = useState<any>([]);
+  const [pagination, setPagination] = useState<any>({ page: 1, limit: 20 });
+  useEffect(() => {
+    fetchData(pagination).then((resultData: any) => {
+      console.log("resultData", resultData);
+      if (resultData.length > 0) {
+        const updatedData = [...data, ...resultData];
+        setData(updatedData);
       }
     });
+  }, [pagination]);
 
-    return updatedSet;
+  const loadMore = () => {
+    const scrollableDiv = scrollableDivRef.current;
+    if (
+      scrollableDiv.scrollTop + scrollableDiv.clientHeight >=
+      scrollableDiv.scrollHeight
+    ) {
+      setPagination({
+        page: pagination.page + 1,
+        limit: 20,
+      });
+    }
   };
 
-  const getData = useCallback(() => {
-    fetchData(pageNumber).then((data) =>
-      setData((prev: any) => addUniqueItems(prev, data))
-    );
-    setPageNumber((prev) => prev + 1);
-  }, [pageNumber]);
-  useEffect(() => {
-    getData();
-  }, [getData]);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.scrollY + window.innerHeight >=
-        document.body.scrollHeight - 5
-      ) {
-        setPageNumber((prev) => prev + 1);
-        getData();
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [pageNumber, getData]);
-
   return (
-    <div className="App">
+    <div className="App" ref={scrollableDivRef} onScroll={loadMore}>
       {data &&
         Array.from(data).map((item: any) => (
           <ProductCard
