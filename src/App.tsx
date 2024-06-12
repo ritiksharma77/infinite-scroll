509ae79f4473af5f18fa38fad4776150054f4f01
ritiksharma77 <73707093+ritiksharma77@users.noreply.github.com>
2024-06-12 20:46:03 +0530

    Infintie scroll (#1)
    * adding infinite scroll
    
    * infinite scroll logic
    
    * Infinite scroll fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { ProductCard } from "./components/product-card";
 import { fetchData } from "./components/fetch-data";
 
 function App() {
-  const [data, setData] = useState<any>(null);
+  const scrollableDivRef = useRef<any>(null);
+  const [data, setData] = useState<any>([]);
+  const [pagination, setPagination] = useState({ page: 1 });
+
   useEffect(() => {
-    fetchData().then((data) => setData(data));
-  }, []);
+    fetchData(pagination.page).then((resultData: any) => {
+      if (resultData.length > 0) {
+        const updatedData = [...data, ...resultData];
+        setData(updatedData);
+      }
+    });
+  }, [pagination]);
+
+  const loadMore = () => {
+    const scrollableDiv = scrollableDivRef.current;
+    if (
+      scrollableDiv.scrollTop + scrollableDiv.clientHeight >=
+      scrollableDiv.scrollHeight
+    ) {
+      setPagination({
+        page: pagination.page + 1,
+      });
+    }
+  };
 
   return (
-    <div className="App">
+    <div className="App" ref={scrollableDivRef} onScroll={loadMore}>
+      <ProductCard key={99999} id="ID" title="TITLE" pageNumber="Page Number" />
       {data &&
-        data.map((item: any) => (
+        Array.from(data).map((item: any) => (
           <ProductCard
             key={item.id}
             id={item.id}
             title={item.title}
-            price={item.price}
+            pageNumber={item.userId}
           />
         ))}
     </div>
--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,16 +1,16 @@
 import "./product-card.css";
 interface productType {
   key: number;
-  id: number;
+  id: string;
   title: string;
-  price: number;
+  pageNumber: string;
 }
-export const ProductCard = ({ id, title, price }: productType) => {
+export const ProductCard = ({ id, title, pageNumber }: productType) => {
   return (
     <div className="product-card-container" key={id}>
       <div className="product-id">{id}</div>
       <div className="product-title">{title}</div>
-      <div className="product-price">${price}</div>
+      <div className="product-price">{pageNumber}</div>
     </div>
   );
 };
